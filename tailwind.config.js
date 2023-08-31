7563e5a0199b8c8ef9c56b9f6d4ffd5502bca8e6
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2023-08-31 23:30:32 +0400

    feat: tailwind configs added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'amiri-regular': 'Amiri-Regular',
+      'amiri-italic': 'Amiri-Italic',
+    },
+    extend: {
+      backgroundImage: {
+        'sword-pattern': 'url(/src/assets/icons/pattern-2.svg)',
+      },
+      screens: {
+        md: '844px',
+      },
+    },
+    colors: {
+      white: '#FFFFFF',
+      'main-gold': '#9E7A43',
+      'main-background': '#202020',
+    },
   },
   plugins: [],
 };
