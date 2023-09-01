d6d0c4befe11843b7a1c0d1c2a8473d7393f6d40
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2023-09-01 09:06:27 +0400

    feat: recreated UI of the page with better config

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import { Header } from './layouts/Header';
+import {
+  SECTION,
+  TEXT_XL,
+  TEXT_SM,
+  TEXT_XS,
+  TEXT_LG,
+  TEXT_MD,
+} from './commonStyles';
+
 import Image1 from './assets/images/image-1.jpeg';
 import Image2 from './assets/images/image-2.jpeg';
 import Image3 from './assets/images/image-3.jpg';
@@ -12,13 +21,13 @@ function App() {
   return (
     <>
       <Header />
-      <div className='flex justify-center mt-14 mb-24'>
-        <img src={Image2} alt='image-1' className={`${IMAGE_CARD_COMMON_STYLES} z-[1] relative left-[150px] rotate-[-15deg] origin-bottom-right grayscale`} />
-        <img src={Image1} alt='image-2' className={`${IMAGE_CARD_COMMON_STYLES} z-[2]`} />
-        <img src={Image3} alt='image-3' className={`${IMAGE_CARD_COMMON_STYLES} z-[3] relative left-[-150px] rotate-[15deg] origin-bottom-left grayscale`} />
-      </div>
-      <div className='xl:w-[1088px] lg:w-[880px] w-[377px] m-auto text-main-gold'>
-        <h2 className='xl:text-[100px] lg:text-[80px] text-[32px] text-center font-amiri-regular'>
+      <div className={SECTION}>
+        <div className='flex justify-center'>
+          <img src={Image2} alt='image-1' className={`${IMAGE_CARD_COMMON_STYLES} z-[1] relative left-[150px] rotate-[-15deg] origin-bottom-right grayscale`} />
+          <img src={Image1} alt='image-2' className={`${IMAGE_CARD_COMMON_STYLES} z-[2]`} />
+          <img src={Image3} alt='image-3' className={`${IMAGE_CARD_COMMON_STYLES} z-[3] relative left-[-150px] rotate-[15deg] origin-bottom-left grayscale`} />
+        </div>
+        <h2 className={`${TEXT_XL} text-center text-main-gold mt-[58px] font-amiri-regular`}>
           <span className='font-amiri-italic'>
             Historical overview on the {' '}
           </span>
@@ -26,13 +35,9 @@ function App() {
             development of Infrastructure in the UAE
           </span>
         </h2>
-        <p className='text-[32px] text-center'>
-          <span className='font-amiri-italic'>Author: </span>
-          <span>UAE National Library and Archives</span>
-        </p>
       </div>
-      <div className='lg:px-[280px] md:px-[120px] px-[16px] text-white text-[24px] mt-[123px]'>
-        <div className='grid md:grid-cols-2 grid-cols-1 grid-rows-2 gap-8'>
+      <div className={`${SECTION} lg:px-[280px] md:px-[120px] px-[16px]`}>
+        <div className={`${TEXT_SM} grid md:grid-cols-2 grid-cols-1 grid-rows-2 gap-8`}>
           <p>
             The United Arab Emirates is famous at home and abroad for having developed its infrastructure
             at breakneck speed. Most of this development has taken place in recent years
@@ -49,25 +54,29 @@ function App() {
             until the end of the 1970s and does so with documents, photographs, videos and newspaper articles.
           </p>
         </div>
-        <img className='w-full xl:mt-[60px] md:mt-[40px] mt-[20px]' src={Image4} alt='cover-image' />
-        <p className='xl:mt-[28px] mt-[16px]'>
+      </div>
+      <div className={`${SECTION} lg:px-[280px] md:px-[120px] px-[16px]`}>
+        <img className='w-full' src={Image4} alt='cover-image' />
+        <p className={`${TEXT_XS} xl:mt-[28px] mt-[16px]`}>
           Report on proposed imporevement of Duba Harbour
           by Sir William Hacrow and partners,January 1955.
           Images reproduced by permission of The National Archives, London, England
         </p>
       </div>
-      <div className='relative mt-[60px]'>
+      <div className={`${SECTION} relative`}>
         <img className='lg:w-[417px] md:w-[324px] w-[205px] m-auto' src={SwordPattern} alt='sword-pattern' />
-        <div className='lg:w-[820px] m-auto text-center absolute bottom-0 left-0 right-0'>
-          <h2 className='text-main-gold lg:text-[60px] text-[24px] font-amiri-regular'>Infrastructure Development in the 1950s</h2>
-          <p className='text-white'>
+        <div className='lg:w-[820px] sm:w-[606px] w-[300px] m-auto text-center absolute bottom-0 left-0 right-0'>
+          <h2 className={`${TEXT_LG} text-main-gold font-amiri-regular`}>Infrastructure Development in the 1950s</h2>
+          <p className={TEXT_MD}>
             Early start of development initiatives, with the establishment of
             Sharjah and Dubai harbours, and the drilling of wells in Al Ain.
           </p>
         </div>
       </div>
-      <div className='my-[40px] py-[76px]'>
-        <Carousel />
+      <div className={`${SECTION}`}>
+        <div className={`${SECTION} bg-black`}>
+          <Carousel />
+        </div>
       </div>
     </>
   );
--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,21 +1,39 @@
 import Slider from 'react-slick';
 import Image from '../../assets/images/image-5.png';
+import Arrow from '../../assets/icons/arrow.svg';
+import { TEXT_XS } from '../../commonStyles';
 
 const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
+  dots: false,
   slidesToShow: 3,
+  arrows: false,
   slidesToScroll: 1,
 };
 
 export const Carousel = () => (
-  <Slider {...settings}>
-    <img src={Image} alt='1' />
-    <img src={Image} alt='2' />
-    <img src={Image} alt='3' />
-    <img src={Image} alt='4' />
-    <img src={Image} alt='5' />
-    <img src={Image} alt='6' />
-  </Slider>
+  <>
+    <Slider {...settings}>
+      <img src={Image} alt='1' />
+      <img src={Image} alt='2' />
+      <img src={Image} alt='3' />
+      <img src={Image} alt='4' />
+      <img src={Image} alt='5' />
+      <img src={Image} alt='6' />
+    </Slider>
+    <div className='flex flex-col items-center'>
+      <span className={`${TEXT_XS} lg:w-[582px] sm:w-[467px] w-full mt-[24px]`}>
+        Report on proposed imporevement of Duba Harbour by Sir William Hacrow and partners,January 1955.
+        Images reproduced by permission of The National Archives, London, England
+      </span>
+      <div className='flex items-center mt-[24px]'>
+        <button>
+          <img src={Arrow} alt='arrow-back' />
+        </button>
+        <span className={`${TEXT_XS} mx-[22px]`}>02/30</span>
+        <button>
+          <img src={Arrow} className='rotate-[180deg]' alt='arrow-back' />
+        </button>
+      </div>
+    </div>
+  </>
 );
