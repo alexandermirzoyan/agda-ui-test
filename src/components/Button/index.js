d785992932a5513ad1bc64a3a11efcff18de6a19
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2023-09-01 09:04:39 +0400

    feat: Button component TODO updated

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,6 @@
 import { ReactComponent as BorderPattern } from '../../assets/icons/pattern.svg';
 
-// TODO - should ask for svg from designer
+// TODO - should ask for svg from designer for using with :before, :after
 export const Button = ({ title }) => (
   <button className='relative inline-flex align-middle justify-center border-0'>
     <BorderPattern className='absolute inset-0 w-full h-full' />
