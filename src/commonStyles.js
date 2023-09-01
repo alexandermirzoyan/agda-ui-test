41d59622afbf914b2586225c66dacb55fae81061
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2023-09-01 09:05:39 +0400

    feat: commonStyles.js constants file created for typography and section

---
+++ src/commonStyles.js
@@ -0,0 +1,7 @@
+export const TEXT_XL = 'xl:text-[100px] lg:text-[80px] md:text-[80px] sm:text-[64px] text-[32px]';
+export const TEXT_LG = 'xl:text-[80px] lg:text-[64px] md:text-[64px] sm:text-[56px] text-[24px]';
+export const TEXT_BASE = 'xl:text-[48px] lg:text-[32px] md:text-[32px] sm:text-[32px] text-[24px]';
+export const TEXT_MD = 'xl:text-[32px] lg:text-[24px] md:text-[24px] sm:text-[24px] text-[16px]';
+export const TEXT_SM = 'xl:text-[24px] lg:text-[20px] md:text-[16px] sm:text-[16px] text-[14px]';
+export const TEXT_XS = 'xl:text-[16px] lg:text-[14px] md:text-[14px] sm:text-[14px] text-[14px]';
+export const SECTION = 'xl:py-[60px] lg:py[40px] md:py-[40px] sm:py-[40px] py-[20px]';
