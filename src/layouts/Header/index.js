730b9bd56069bea5bd28a53fe35d9c590584eded
Alexandr Mirzoyan <alexandr.mirz12@gmail.com>
2023-09-03 17:47:18 +0400

    fix(Header): minor improvements

--- src/layouts/Header/index.js
+++ src/layouts/Header/index.js
@@ -6,11 +6,11 @@ const HEADER_PADDINGS = 'py-4 lg:ps-[54px] lg:pe-[64px] md:ps-[64px] md:pe-[64px
 export const Header = () => (
   <header className={`flex items-center justify-between ${HEADER_PADDINGS}`}>
     <img src={logo} alt='logo' className='lg:w-[242px] w-[165px]' />
-    <nav className={`${TEXT_XS} sm:block hidden`}>
-      <a className='lg:mr-14 mr-12 text-main-gold' href='#'>Eng</a>
-      <a className='lg:mr-14 mr-12' href='#'>The archives</a>
-      <a className='lg:mr-14 mr-12' href='#'>Online Exhibitions</a>
-      <a className='lg:mr-14 mr-12' href='#'>About us</a>
+    <nav className={`${TEXT_XS} uppercase sm:block hidden`}>
+      <a className='md:mr-[60px] mr-[48px] text-main-gold' href='#'>Eng</a>
+      <a className='md:mr-[60px] mr-[48px]' href='#'>The archives</a>
+      <a className='md:mr-[60px] mr-[48px]' href='#'>Online Exhibitions</a>
+      <a className='md:mr-[60px] mr-[48px]' href='#'>About us</a>
       <a href='#'>Register</a>
     </nav>
     <button className='w-[18px] block sm:hidden'>
